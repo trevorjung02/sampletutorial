23291b91a8fc225b146e751b7a84dbbce0caed2c
Trevor Jung <trevorjung02@gmail.com>
2020-07-21 19:36:12 -0700

    Second commit

--- index.js
+++ index.js
@@ -3,28 +3,6 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-	/*if(req.url === '/') {
-		fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
-			if(err) throw err;
-			res.writeHead(200, {'Content-Type': 'text/html'});
-			res.end(content);
-		})
-	}
-	if(req.url === '/about') {
-		fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
-			if(err) throw err;
-			res.writeHead(200, {'Content-Type': 'text/html'});
-			res.end(content);
-		})
-	}
-	if(req.url === '/api/users') {
-		const users = [
-			{ name: 'Bob Smith', age: 40},
-			{ name: 'John Doe', age: 30}
-		];
-		res.writeHead(200, {'Content-Type': 'application/json'});
-		res.end(JSON.stringify(users));
-	}*/
 	let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
 	let extname = path.extname(filePath);
 	let contentType = 'text/html';
